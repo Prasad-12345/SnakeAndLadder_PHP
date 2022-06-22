0309f770dd01ab332ed9ea66ea5e056243b835bb
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-22 17:40:02 +0530

    Added: Player rolls dice to get number between 1 to 6

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -2,11 +2,24 @@
     class SankeAndLadder{
         //variable
         private $startPosition = 0;
+        private $previousPosition;
 
         //welcome message
         public function welcome(){
             echo "Welcome to the Snake And Ladder game \n";
-            echo "Player starts from $this->startPosition";
+            echo "Player starts from $this->startPosition \n";
+        }
+
+        //function to generate random number between 1 to 6
+        public function rollDice(){
+            $this->previousPosition = $this->startPosition;
+            $this->startPosition = rand(1, 6);
+            echo "Previous Position :" . $this->previousPosition . " " . "StartPosition :" . $this->startPosition;
         }
     }
+
+    //object
+    $game = new SankeAndLadder;
+    $game->welcome();
+    $game->rollDice();
 ?>
