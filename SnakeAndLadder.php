2642158a6bdd2f4be9560c4fefccb6e67043a1cf
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-06-23 08:57:06 +0530

    Added: Player checks for options like no play, ladder or snake

--- SnakeAndLadder.php
+++ SnakeAndLadder.php
@@ -12,14 +12,43 @@
 
         //function to generate random number between 1 to 6
         public function rollDice(){
+            //$this->previousPosition = $this->startPosition;
+            $diceNum = rand(1, 6);
+            //echo "Previous Position :" . $this->previousPosition . " " . "StartPosition :" . $this->startPosition;
+            return $diceNum;
+        }
+
+        //Function to generate random number between 1 to 3
+        public function option(){
+            return rand(1, 3);
+        }
+
+        //Function to decide next position of player
+        public function nextMove($diceNum){    
+            $choice = $this->option();
             $this->previousPosition = $this->startPosition;
-            $this->startPosition = rand(1, 6);
-            echo "Previous Position :" . $this->previousPosition . " " . "StartPosition :" . $this->startPosition;
+            switch($choice){
+                case 1:
+                    $this->startPosition = $this->startPosition;
+                    echo "Previous Position :" . $this->previousPosition . " " . "StartPosition :" . $this->startPosition;
+                    break;
+
+                case 2:
+                    $this->startPosition += $diceNum;
+                    echo "Previous Position :" . $this->previousPosition . " " . "StartPosition :" . $this->startPosition;
+                    break;
+
+                case 3:
+                    $this->startPosition -= $diceNum;
+                    echo "Previous Position :" . $this->previousPosition . " " . "StartPosition :" . $this->startPosition;
+                    break;
+            }
         }
     }
 
     //object
     $game = new SankeAndLadder;
     $game->welcome();
-    $game->rollDice();
+    $diceNum = $game->rollDice();
+    $game->nextMove($diceNum);
 ?>
